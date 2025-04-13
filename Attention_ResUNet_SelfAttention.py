76f9dc50985c98fa5d0fd8946926b41ada069f4a
Niblic <77663934+Niblic@users.noreply.github.com>
2025-04-13 16:16:05 +0200

    Update Attention_ResUNet_SelfAttention.py

--- Attention_ResUNet_SelfAttention.py
+++ Attention_ResUNet_SelfAttention.py
@@ -15,7 +15,7 @@ import tensorflow.keras.backend as K
 
 tf.keras.backend.set_image_data_format('channels_last')
 # input data
-INPUT_SIZE = 256
+INPUT_SIZE = 512
 INPUT_CHANNEL = 3 # 1-grayscale, 3-RGB scale
 OUTPUT_MASK_CHANNEL = 1
 
@@ -37,42 +37,8 @@ def squeeze_and_excitation_block(input_X, reduction_ratio=16):
     excitation = layers.Dense(units=channels, activation='sigmoid')(excitation)
     
     return input_X * excitation
-
-def conv_block(x, filters):
-    """
-    Standard Convolution Block: 2x Convolution + BatchNorm + ReLU
-    """
-    x = layers.Conv2D(filters, (FILTER_SIZE, FILTER_SIZE), padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
     
-    x = layers.Conv2D(filters, (FILTER_SIZE, FILTER_SIZE), padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
     
-    return x
-
-def upsample_block(x, filters):
-    """
-    Upsampling Block: UpSampling2D + Convolution zur Kanalanpassung
-    """
-    x = layers.UpSampling2D((UP_SAMP_SIZE, UP_SAMP_SIZE))(x)
-    x = layers.Conv2D(filters, (UP_SAMP_SIZE, UP_SAMP_SIZE), padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
-    
-    return x
-
-def gating_signal(input_tensor, out_size):
-    """
-    Gating Signal: 1x1 Convolution zum Anpassen der Kanäle
-    """
-    gating = layers.Conv2D(out_size, kernel_size=(1, 1), padding="same")(input_tensor)
-    gating = layers.BatchNormalization()(gating)
-    gating = layers.Activation('relu')(gating)
-    return gating
-
-
 class SelfAttentionBlock(layers.Layer):
     def __init__(self, **kwargs):
         super(SelfAttentionBlock, self).__init__(**kwargs)
@@ -163,11 +129,11 @@ def Attention_ResUNet_SelfAttention(input_shape=( 512 , 512 , 9  )):
     # Decoder-Pfad mit Self-Attention-Blocks und Skip-Connections
     u7 = upsample_block(gating, NUM_FILTER * 16)
     # Downsample before self-attention
-    down_sa7 = layers.MaxPooling2D((2, 2))(u7)
-    sa7 = SelfAttentionBlock()(down_sa7)
+    #down_sa7 = layers.MaxPooling2D((2, 2))(u7)
+    sa7 = SelfAttentionBlock()(u7)
     # Upsample after self-attention
-    up_sa7 = layers.UpSampling2D((2, 2))(sa7)
-    c7 = conv_block(layers.concatenate([up_sa7, c5]), NUM_FILTER * 16)
+    #up_sa7 = layers.UpSampling2D((2, 2))(sa7)
+    c7 = conv_block(layers.concatenate([sa7, c5]), NUM_FILTER * 16)
 
     u8 = upsample_block(c7, NUM_FILTER * 8)
     sa8 = SelfAttentionBlock()(u8) # Apply at original resolution for others
@@ -178,19 +144,14 @@ def Attention_ResUNet_SelfAttention(input_shape=( 512 , 512 , 9  )):
     c9 = conv_block(layers.concatenate([sa9, c3]), NUM_FILTER * 4)
 
     u10 = upsample_block(c9, NUM_FILTER * 2)
-    # sa10 = SelfAttentionBlock()(u10) # OOM Issue removed 
-    # c10 = conv_block(layers.concatenate([sa10, c2]), NUM_FILTER * 2)
-    c10 = conv_block(layers.concatenate([u10, c2]), NUM_FILTER * 2)
+    se10 = squeeze_and_excitation_block(u10)
+    c10 = conv_block(layers.concatenate([se10, c2]), NUM_FILTER * 2)
 
     u11 = upsample_block(c10, NUM_FILTER)
-    # sa11 = SelfAttentionBlock()(u11)   # OOM Issue removed 
-    # c11 = conv_block(layers.concatenate([sa11, c1]), NUM_FILTER)
-    c11 = conv_block(layers.concatenate([u11, c1]), NUM_FILTER)
+    se11 = squeeze_and_excitation_block(u11)
+    c11 = conv_block(layers.concatenate([se11, c1]), NUM_FILTER)
     # Ausgabeschicht
     conv_final = layers.Conv2D(1, (1, 1), activation="sigmoid")(c11)
-
-    conv_final = layers.BatchNormalization(axis=3)(conv_final)
-    conv_final = layers.Activation('sigmoid')(conv_final)
 
     model = tf.keras.models.Model(inputs=[inputs], outputs=conv_final , name="Attention_ResUNet_SelfAttention")
     return model
